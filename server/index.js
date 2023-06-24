8c044a2b11ea52cf9053ff2041dde96b52f727ec
kennethpole6 <polekenneth11@gmail.com>
2023-06-24 09:39:22 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  //joining room/groups
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  //received the message coming from the client
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+
+    // socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server is running...");
+});
